8debafb280231a6126e7e44ed81e4d0b773c1dee
Sahil Grover <sahilgrover67@gmail.com>
2017-02-16 17:18:18 +0530

    Adds C++ File

---
+++ Interval-Tree.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#typedef pair<int,int> interval;
+
+struct Node  //Interval Tree Node
+{
+	interval i;
+	int Max;
+	ITNode *left,*right;
+};
+
+Node * make_node (interval i) // Makes a new node
+{
+
+}
+
+Node * insert_node (Node *root, interval i)
+{
+
+}
+
+interval * search (Node *root,interval i)
+{
+
+}
+
+void delete_node (interval i)
+{
+
+} 
+
+int main()
+{
+	
+}
